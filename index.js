e523af1004dadf3d8df100e7bd435dd3a53a0583
mako314 <119079347+mako314@users.noreply.github.com>
2023-05-05 11:48:11 -0400

    change element text before dom loads

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const ele = document.getElementById("text")
+    ele.textContent = "This is really cool!"
+    console.log(ele)
+  });
+
